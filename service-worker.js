a651ebb652cda9a70f3b195f6130549ef5c49ef6
Ani Sunaryati <32156532+customtheme@users.noreply.github.com>
2018-05-23 21:58:54 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 var cacheName = 'offline-fallback-cache-__VERSION__';
 
 var requiredCacheFiles = [
- location.origin + '/p/offlinefallback.html'
+ location.origin + 'https://ramaibeud.blogspot.com/p/offlinefallback.html'
 ];
 
 self.addEventListener('install', function(event){
